2f524efd3f5bbab8ad560ea359a8b6ef81a0a59a
xxxDNxxx <beerurban@gmail.com>
2019-08-21 18:36:46 +0700

    Add action to Button

--- app/src/main/java/buu/informatics/s59160379/aboutme/MainActivity.kt
+++ app/src/main/java/buu/informatics/s59160379/aboutme/MainActivity.kt
@@ -17,6 +17,24 @@ class MainActivity : AppCompatActivity() {
         findViewById<Button>(R.id.done_button).setOnClickListener {
             addNickname(it)
         }
+        findViewById<TextView>(R.id.nickname_text).setOnClickListener {
+            updateNickname(it)
+        }
+    }
+
+    private fun updateNickname(view: View) {
+        val editText = findViewById<EditText>(R.id.nickname_edit)
+        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+        editText.visibility = View.VISIBLE
+        nicknameTextView.visibility = View.GONE
+
+        val doneButton = findViewById<Button>(R.id.done_button)
+        doneButton.visibility = View.VISIBLE
+
+        editText.requestFocus()
+
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.showSoftInput(editText,0)
     }
 
     private fun addNickname(view: View) {
